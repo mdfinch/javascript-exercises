4c2d3d8a6a3099d9a2d0712cea0500d34c75081f
mdfinch <michael.donovan.finch@gmail.com>
2022-04-14 12:56:24 -0700

    Completed excersie 03_reverseString
    Added two different functions that complete the exercise. One with
    arrays, one without.
    
    Enabled all tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+// const reverseString = function(string) {
+//     let word = '';
+//     for (let index = string.length - 1; index >= 0; index--) {
+//         word += string.charAt(index)
+//         console.log(word)
+//     }
+//     return word   
+// };
 
-};
+const reverseString = function(string) {
+    let word = '';
+    let array = string.split('');
+    for (let index = array.length - 1; index >= 0; index--) {
+                word += array[index]
+    }
+    return word
+}
 
 // Do not edit below this line
 module.exports = reverseString;
