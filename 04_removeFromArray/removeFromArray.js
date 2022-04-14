b5ca2d577b75ecac0b40f3e0661a93ec1197c4c1
mdfinch <michael.donovan.finch@gmail.com>
2022-04-14 16:07:02 -0700

    Completed exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    
+    return newArray;
+  };
 
-};
+  removeFromArray([1,2,3,4], 3, 4)
 
-// Do not edit below this line
 module.exports = removeFromArray;
