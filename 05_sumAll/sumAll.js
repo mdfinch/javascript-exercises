45c7a55b352cd71bb9b95f32ff46f6bce2d38c8a
mdfinch <michael.donovan.finch@gmail.com>
2022-04-15 15:27:03 -0700

    Completed excercise 05_sumAll
    First created sumAll function with a for loop. Then added the type of
    check to make sure non numbers returned an error message. Then added
    a check for negative numbers. Last was to switch params 1 and 2 if 1
    was higher then 2.
    
    Can refactor to limit amount of nested conditional statements and
    repeated for loop.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(p1, p2) {
+    let sum = 0; 
+    if(typeof(p1) !== typeof(1) || typeof(p2) !== typeof(1)) {
+        return "ERROR"
+    } else if (p1 < 0 || p2 < 0) {
+        return "ERROR"
+    } else {
+        if(p1 > p2) {
+            let arg1 = p2;
+            let arg2 = p1;
+            for(let x = arg1; x <= arg2; x++) {
+                sum += x
+            }
+            return sum
+        } else {
+            for(let x = p1; x <= p2; x++) {
+                sum += x
+            }
+            return sum
+        }
+    }
 };
 
+sumAll(1, 4)
 // Do not edit below this line
 module.exports = sumAll;
