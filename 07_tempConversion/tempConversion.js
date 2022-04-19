c0857563f17fc945f393c0a4d53f2e0a0362610d
mdfinch <michael.donovan.finch@gmail.com>
2022-04-19 13:27:13 -0700

    Completed exercise 07_tempConversion
    Added forumalas from wikipedia. Looked up how to manually round.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(number) {
+  let farenheight = (number - 32) * (5/9);
+  console.log(farenheight)
+  let answer = Math.round(farenheight * 10) / 10
 
+  return answer
 };
 
-const ctof = function() {
+const ctof = function(num) {
+  let celcius = (num * 9/5) + 32;
+  let ans = Math.round(celcius * 10) / 10
 
+  return ans
 };
 
 // Do not edit below this line
